edd7f7b970add6e93fe74b935593b13a61434937
7vik-git <sathviklingamsetti@gmail.com>
2025-04-04 12:34:56 +0530

    Added Strings

---
+++ src/strings/CreateSubstring.java
@@ -0,0 +1,22 @@
+package strings;
+
+import java.util.Scanner;
+public class CreateSubstring {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String stringOne = input.nextLine();
+        System.out.println("Enter start and end index for substring: ");
+        int startIndex = input.nextInt();
+        int endIndex = input.nextInt();
+        String subString="";
+        for(int i=startIndex;i<endIndex;i++){
+            subString+=stringOne.charAt(i);
+        }
+        System.out.println("The substring is : "+subString);
+        System.out.println("The substring using the method substring()");
+        System.out.println(stringOne.substring(startIndex, endIndex));
+    }
+}
+
+
---
+++ src/strings/LengthOfString.java
@@ -0,0 +1,29 @@
+package strings;
+import java.util.Scanner;
+public class LengthOfString {
+    public static int FindLengthOfString(String string) {
+        int count=0;
+        try{
+            while(true){
+                string.charAt(count);
+                count++;
+            }
+        }
+        catch(StringIndexOutOfBoundsException e){
+            System.out.println(e.getMessage());
+            return count;
+        }
+    }
+    public static int LengthOfStringUsingBuiltInMethod(String string){
+        return string.length();
+    }
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("Enter a string:");
+        String string=scanner.nextLine();
+        System.out.println("Using iteration: "+FindLengthOfString(string));
+        System.out.println("Using built-in method: "+LengthOfStringUsingBuiltInMethod(string));
+        scanner.close();
+    }
+}
+
